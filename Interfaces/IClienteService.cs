6d8d569960695df2b9fe825c86403ca87aee3d96
Takiguchii <alexandre.pupim360@gmail.com>
2025-08-15 19:21:57 -0300

    Fix: Corrigindo bugs no 'IClienteService'

--- Interfaces/IClienteService.cs
+++ Interfaces/IClienteService.cs
@@ -1,9 +1,10 @@
+using FarmaApi.DTOs;
 using FarmaApi.Models;
 
 namespace FarmaApi.Interfaces;
 
 public interface IClientService
 {
-    public List<Client> GetClientes();
-    public Client CreateClient();
+    public List<Client> GetClients();
+    public Client CreateClient(CreateClientDTO dto);
 }
