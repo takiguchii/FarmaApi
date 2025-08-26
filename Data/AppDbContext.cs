31c7b6f3bca518d5b93552757638cfc0ac41e476
Takiguchii <alexandre.pupim360@gmail.com>
2025-08-26 19:52:47 -0300

    Feat: Create config DbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,15 @@
+using FarmaApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace FarmaApi.Data;
 
-public class AppDbContext
+public class AppDbContext : DbContext
 {
-    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Sale> Sales { get; set; }
 }
