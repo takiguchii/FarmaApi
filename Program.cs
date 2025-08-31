26603419076180613c5c31e438fe42b83117ca10
Takiguchii <alexandre.pupim360@gmail.com>
2025-08-31 10:45:06 -0300

    Fix: Corrigindo erros na chamada de independencia do program.cs

--- Program.cs
+++ Program.cs
@@ -1,17 +1,26 @@
-using FarmaApi.Data;
 using FarmaApi.Interfaces;
 using FarmaApi.Service;
+using FarmaApi.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
 var connectionString = builder.Configuration.GetConnectionString("MySqlConnection");
-builder.Services.AddControllers();
 
 builder.Services.AddDbContext<FarmaApiContext>(options =>
 {
     options.UseMySql(connectionString,
         ServerVersion.AutoDetect(connectionString));
 });
+
+builder.Services.AddScoped<IClientService, ClientService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ISaleService, SaleService>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
