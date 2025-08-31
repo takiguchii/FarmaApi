ed5009699f5e7805c052e3e9cf248341ea3b27ed
Takiguchii <alexandre.pupim360@gmail.com>
2025-08-31 10:30:00 -0300

    Refactor: Adicionando Logica do banco ao SaleService

--- Service/SaleService.cs
+++ Service/SaleService.cs
@@ -1,63 +1,58 @@
 using FarmaApi.DTOs;
 using FarmaApi.Interfaces;
 using FarmaApi.Models;
+using FarmaApi.Data; 
 using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore; 
 
 namespace FarmaApi.Service;
 
 public class SaleService : ISaleService
 {
-    private readonly IClientService _clientService;
-    private readonly IProductService _productService;
+    private readonly FarmaApiContext _dbContext;
     
-    private readonly List<Sale> _sales = new List<Sale>();
-    private int _nextId = 1;
-
-    public SaleService(IClientService clientService, IProductService productService)
+    public SaleService(FarmaApiContext dbContext)
     {
-        _clientService = clientService;
-        _productService = productService;
+        _dbContext = dbContext;
     }
     
     public Sale CreateSale(CreateSaleDTO dto)
     {
-        // 1. Validar se o cliente comprador é válido
-        var clients = _clientService.GetClients();
-        var client = clients.FirstOrDefault(c => c.id == dto.ClientId);
+        var client = _dbContext.Clients.FirstOrDefault(c => c.id == dto.ClientId);
         if (client == null)
         {
             throw new InvalidOperationException("Cliente não encontrado.");
         }
 
-        // 2. Validar se produto comprado é válido
-        var products = _productService.GetProducts();
-        var product = products.FirstOrDefault(p => p.id == dto.ProductId);
+        var product = _dbContext.Products.FirstOrDefault(p => p.id == dto.ProductId);
         if (product == null)
         {
             throw new InvalidOperationException("Produto não encontrado.");
         }
-
-        // 3. Validar se a quantidade é válida 
+        
         if (dto.Quantity <= 0)
         {
             throw new InvalidOperationException("Quantidade da venda deve ser maior que zero.");
         }
-
-        // Se tudo for aprovado a venda é criada
+        
         Sale newSale = new Sale
         {
-            Id = _nextId++,
             Client = client,
             Product = product,
             Quantity = dto.Quantity,
         };
-
-        _sales.Add(newSale);
+        
+        _dbContext.Sales.Add(newSale);
+        _dbContext.SaveChanges();
         return newSale;
     }
 
     public List<Sale> GetSales()
     {
-        return _sales;
+        return _dbContext.Sales
+            .Include(s => s.Client)
+            .Include(s => s.Product)
+            .ToList();
     }
 }
